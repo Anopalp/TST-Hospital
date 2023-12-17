831ed9f21f9852856275e24da7e3e70978ad8798
Aliefnaufal <18221105@std.stei.itb.ac.id>
2023-12-17 09:07:29 +0700

    tipis2

--- hospital-app/app/Views/registrasi.php
+++ hospital-app/app/Views/registrasi.php
@@ -6,19 +6,32 @@
         <link href="https://fonts.googleapis.com/css2?family=Karla&family=Libre+Baskerville&family=Roboto:wght@100&display=swap" rel="stylesheet">
     </head>
     <body>
-        <p>Menu :
-            <a href="/">Dashboard</a>
-            <a href="/registrasi">Registrasi</a>
-            <a href="/riwayatRegistrasi">Riwayat Registrasi</a>
-            <a href="/penjualanObat">Penjualan Obat</a>
-            <a href="/riwayatPenjualanObat">Riwayat Penjualan Obat</a>
-        </p>
+        <div class="navbar">
+            <div class="home-icon">
+                <a href="/">Dashboard</a>
+            </div>
 
+            <div class="gap">
+
+            </div>
+
+            <div class="other-nav">
+                <a href="/registrasi">Registrasi</a>
+                <a href="/riwayatRegistrasi">Riwayat Registrasi</a>
+                <a href="/penjualanObat">Penjualan Obat</a>
+                <a href="/riwayatPenjualanObat">Riwayat Penjualan Obat</a>
+            </div>
+        </div>
+
+        <h2>Pilih Asuransi</h2>
+        
         <div class="card-asuransi">
-            <h2>Pilih Asuransi</h2>
-            <div class="opsi-asuransi">
-                <a href="/registrasiBPJS">BPJS</a>
-                <a href="/registrasiNonBPJS">Non BPJS</a>
+            <div class="inner-card" onclick="window.location.href='/registrasiBPJS'">
+                <div class="card-text">BPJS</div>
+            </div>
+            
+            <div class="inner-card" onclick="window.location.href='/registrasiNonBPJS'">
+                <div class="card-text">Non BPJS</div>
             </div>
         </div>
     </body>
