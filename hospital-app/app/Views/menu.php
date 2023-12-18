7cdaba968a94f1578b363ef2e08267e052a2604f
Aliefnaufal <18221105@std.stei.itb.ac.id>
2023-12-18 11:20:46 +0700

    tipis2

--- hospital-app/app/Views/menu.php
+++ hospital-app/app/Views/menu.php
@@ -12,32 +12,44 @@
 
         <div class="card-main">
             <div class="inner-card" onclick="window.location.href='/registrasi'">
-                <div class="card-image"></div>
+                <div class="card-image">
+                    <img src="/images/registrasi.png" alt="">
+                </div>
                 <div class="card-text">Registrasi</div>    
             </div>
             
             <div class="inner-card" onclick="window.location.href='/riwayatRegistrasi'">
-                <div class="card-image"></div>
+                <div class="card-image">
+                    <img src="/images/history.png" alt="">
+                </div>
                 <div class="card-text">Riwayat Registrasi</div>    
             </div>
             
             <div class="inner-card" onclick="window.location.href='/penjualanObat'">
-                <div class="card-image"></div>
+                <div class="card-image">
+                    <img src="/images/request.png" alt="">
+                </div>
                 <div class="card-text">Request Obat</div>    
             </div>
 
             <div class="inner-card" onclick="window.location.href='/riwayatPenjualanObat'">
-                <div class="card-image"></div>
+                <div class="card-image">
+                    <img src="/images/history.png" alt="">
+                </div>
                 <div class="card-text">Riwayat Request Obat</div>    
             </div>
 
             <div class="inner-card" onclick="window.location.href='/logout'">
-                <div class="card-image"></div>
+                <div class="card-image">
+                    <img src="/images/logout.png" alt="">
+                </div>
                 <div class="card-text">Log Out</div>
             </div>
 
             <div class="inner-card" onclick="window.location.href='/about'">
-                <div class="card-image"></div>
+                <div class="card-image">
+                    <img src="/images/about.png" alt="">
+                </div>
                 <div class="card-text">About</div>
             </div>
         </div>
